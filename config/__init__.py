dbff419e9353c301b013421fcda1414eec465da6
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2022-12-18 22:15:05 +0700

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    API_ID = int( os.getenv("api_id","1234") )
-    API_HASH = os.getenv("api_hash","21ab7cb0a453b5e60016dc7bbeb71cb")
-    CHANNEL = int( os.getenv("channel_files_chat_id","-1001601419165") )
-    CHANNEL_USERNAME = os.getenv("channel_username","UserLandapp")
-    TOKEN = os.getenv("token","1408542385:AAEatXPbSubsYmlW8kPTUAr")
-    DOMAIN  = os.getenv("domain","http://localhost")
+    API_ID = int( os.getenv("api_id","14672956") )
+    API_HASH = os.getenv("api_hash","115e8242ea0423893160bb61a9e05eab")
+    CHANNEL = int( os.getenv("channel_files_chat_id","-1001866017806") )
+    CHANNEL_USERNAME = os.getenv("channel_username","kblois")
+    TOKEN = os.getenv("token","5821948686:AAEsgkTwB6FvyTQfj-fMfA7etHM_AM_7U7w")
+    DOMAIN  = os.getenv("domain","https://stream-cloud-production-cbb4.up.railway.app")
